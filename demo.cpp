3a07365c0c3a6b89458578f4c974f8525ce7273d
kraxarn <me@kraxarn.com>
2020-03-24 22:54:14 +0100

    Initial demo application

--- demo.cpp
+++ demo.cpp
@@ -1,1 +1,28 @@
- 
+#include "QVorbis.hpp"
+
+#include <QDebug>
+#include <QCoreApplication>
+
+int main(int argc, char **argv)
+{
+	// Initialize Qt
+	QCoreApplication app(argc, argv);
+
+	// Check correct usage
+	if (argc != 2)
+	{
+		qWarning() << "qvorbis_demo <file.ogg>";
+		return 1;
+	}
+
+	// Play
+	QVorbis vorbis;
+	if (!vorbis.play(QString(argv[1])))
+	{
+		qWarning() << "file failed to load";
+		return 1;
+	}
+
+	// Run main thing
+	return QCoreApplication::exec();
+}
